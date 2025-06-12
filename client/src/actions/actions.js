4ce5bbcab261bcb7e17ec0311f78bede7877043a
Israel Akpadja <drimmaculate147@gmail.com>
2025-06-12 01:32:39 +0000

    changed action types into constants

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -6,59 +6,75 @@ import {
   likeAPost,
 } from "../api/api";
 
+import {
+  FETCH_REQUEST,
+  FETCH_SUCCESS,
+  FETCH_FAILURE,
+  CREATE_REQUEST,
+  CREATE_SUCCESS,
+  CREATE_FAILURE,
+  UPDATE_REQUEST,
+  UPDATE_SUCCESS,
+  UPDATE_FAILURE,
+  LIKE_SUCCESS,
+  DELETE_REQUEST,
+  DELETE_SUCCESS,
+  DELETE_FAILURE,
+} from "../constants/actionTypes";
+
 // Action creators
 export const getPosts = () => async (dispatch) => {
   try {
-    dispatch({ type: "FETCH_REQUEST" });
+    dispatch({ type: FETCH_REQUEST });
 
     const { data } = await fetchPosts();
 
-    dispatch({ type: "FETCH_SUCCESS", payload: data });
+    dispatch({ type: FETCH_SUCCESS, payload: data });
   } catch (error) {
     console.log(error);
 
-    dispatch({ type: "FETCH_FAILURE", payload: error.message });
+    dispatch({ type: FETCH_FAILURE, payload: error.message });
   }
 };
 
 export const createPost = (newPost) => async (dispatch) => {
   try {
-    dispatch({ type: "CREATE_REQUEST" });
+    dispatch({ type: CREATE_REQUEST });
 
     const { data } = await createAPost(newPost);
 
-    dispatch({ type: "CREATE_SUCCESS", payload: data });
+    dispatch({ type: CREATE_SUCCESS, payload: data });
   } catch (error) {
     console.log(error);
 
-    dispatch({ type: "CREATE_FAILURE", payload: error.message });
+    dispatch({ type: CREATE_FAILURE, payload: error.message });
   }
 };
 
 export const updatePost = (id, post) => async (dispatch) => {
   try {
-    dispatch({ type: "UPDATE_REQUEST" });
+    dispatch({ type: UPDATE_REQUEST });
 
     const { data } = await updateAPost(id, post);
 
-    dispatch({ type: "UPDATE_SUCCESS", payload: data });
+    dispatch({ type: UPDATE_SUCCESS, payload: data });
   } catch (error) {
     console.log(error);
 
-    dispatch({ type: "UPDATE_FAILURE", payload: error.message });
+    dispatch({ type: UPDATE_FAILURE, payload: error.message });
   }
 };
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    dispatch({ type: "DELETE_REQUEST" });
+    dispatch({ type: DELETE_REQUEST });
     await deleteAPost(id);
 
-    dispatch({ type: "DELETE_SUCCESS", payload: id });
+    dispatch({ type: DELETE_SUCCESS, payload: id });
   } catch (error) {
     console.log(error);
 
-    dispatch({ type: "DELETE_FAILURE", payload: error.message });
+    dispatch({ type: DELETE_FAILURE, payload: error.message });
   }
 };
 
@@ -66,7 +82,7 @@ export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await likeAPost(id);
 
-    dispatch({ type: "LIKE_SUCCESS", payload: data });
+    dispatch({ type: LIKE_SUCCESS, payload: data });
   } catch (error) {
     console.log(error);
   }
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,3 +1,19 @@
+import {
+  FETCH_REQUEST,
+  FETCH_SUCCESS,
+  FETCH_FAILURE,
+  CREATE_REQUEST,
+  CREATE_SUCCESS,
+  CREATE_FAILURE,
+  UPDATE_REQUEST,
+  UPDATE_SUCCESS,
+  UPDATE_FAILURE,
+  LIKE_SUCCESS,
+  DELETE_REQUEST,
+  DELETE_SUCCESS,
+  DELETE_FAILURE,
+} from "../constants/actionTypes";
+
 // reducers/posts.js
 const initialState = {
   posts: [],
@@ -9,28 +25,28 @@ const initialState = {
 
 const postsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "FETCH_REQUEST":
+    case FETCH_REQUEST:
       return { ...state, isLoading: true };
-    case "FETCH_SUCCESS":
+    case FETCH_SUCCESS:
       return { ...state, posts: action.payload, isLoading: false };
-    case "FETCH_FAILURE":
+    case FETCH_FAILURE:
       return { ...state, isLoading: false };
-    case "CREATE_REQUEST": {
+    case CREATE_REQUEST: {
       return { ...state, isCreating: true };
     }
-    case "CREATE_SUCCESS":
+    case CREATE_SUCCESS:
       return {
         ...state,
         posts: [...state.posts, action.payload],
         isCreating: false,
       };
-    case "CREATE_FAILURE": {
+    case CREATE_FAILURE: {
       return { ...state, isCreating: false };
     }
-    case "UPDATE_REQUEST": {
+    case UPDATE_REQUEST: {
       return { ...state, isUpdating: true };
     }
-    case "UPDATE_SUCCESS":
+    case UPDATE_SUCCESS:
       return {
         ...state,
         posts: state.posts.map((post) =>
@@ -38,10 +54,10 @@ const postsReducer = (state = initialState, action) => {
         ),
         isUpdating: false,
       };
-    case "UPDATE_FAILURE": {
+    case UPDATE_FAILURE: {
       return { ...state, isUpdating: false };
     }
-    case "LIKE_SUCCESS": {
+    case LIKE_SUCCESS: {
       let liked = false;
       const likedPosts = state.posts.map((post) => {
         if (post._id === action.payload._id) {
@@ -58,16 +74,16 @@ const postsReducer = (state = initialState, action) => {
         posts: liked ? likedPosts : state.posts,
       };
     }
-    case "DELETE_REQUEST": {
+    case DELETE_REQUEST: {
       return { ...state, isDeleting: true };
     }
-    case "DELETE_SUCCESS":
+    case DELETE_SUCCESS:
       return {
         ...state,
         posts: state.posts.filter((post) => post._id !== action.payload),
         isDeleting: false,
       };
-    case "DELETE_FAILURE": {
+    case DELETE_FAILURE: {
       return { ...state, isDeleting: false };
     }
     default:
